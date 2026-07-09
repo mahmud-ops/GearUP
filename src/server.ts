2dcd87e55d80a1dc230542fcbbbd43c7018a6879
mahmud-ops <abdullahmahmud01798@gmail.com>
2026-07-09 23:27:02 +0600

    feat(db): Define data models

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import express, { type Express, type Request, type Response } from "express";
-import config from "./config";
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const port = config.port;
 
 async function main() {
   try {
+    await prisma.$connect();
+    console.log(`Database connected successfully`);
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
   } catch (error) {
+    await prisma.$disconnect()
     console.log(error);
     process.exit(1);
   }
