f8a4fcf0a0319f6a54800a06b1f79753871d6c21
mahmud-ops <abdullahmahmud01798@gmail.com>
2026-08-10 22:25:00 +0600

    fix(api): update CORS origin to production URL

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // or your frontend URL
+    origin: "https://gearup-kappa.vercel.app", // frontend URL
     credentials: true,
   })
 );
